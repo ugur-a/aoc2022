05c2a21d0a0346e4088c3f15c820daf78e894f32
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-02-26 11:01:24 +0100

    initial commit

--- src/d7.rs
+++ src/d7.rs
@@ -1,64 +1,51 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    path::{Path, PathBuf},
+};
 
 use itertools::Itertools;
 
-struct Directory {
-    subdirs: Vec<Box<str>>,
-    own_size: u32,
-}
-
-impl Directory {
-    fn build(subdirs: Vec<Box<str>>, own_size: u32) -> Self {
-        Self { subdirs, own_size }
-    }
-}
-
 pub fn p1(file: &str) -> u32 {
     let navigations = &file[2..];
-    let mut dir_stack = Vec::new();
-    let mut filesystem: HashMap<&str, Directory> = HashMap::new();
+    let mut current_path = PathBuf::new();
+    let mut files_with_sizes: HashMap<PathBuf, u32> = HashMap::new();
+    let upper_bound = 100_000u32;
 
     for input_and_output in navigations.split("\n$ ") {
-        match input_and_output.split_whitespace().collect_vec().as_slice() {
-            ["cd", dir_name] => match *dir_name {
-                ".." => drop(dir_stack.pop()),
-                dir_name => dir_stack.push(dir_name),
-            },
-            _ => {
-                let (_input, output) = input_and_output.split_once("\n").unwrap();
-                let mut subdirs = Vec::new();
-                let mut own_size = 0;
-                for line in output.lines() {
-                    match line.split_once(" ").unwrap() {
-                        ("dir", dir_name) => subdirs.push(Box::from(dir_name)),
-                        (file_size, _file_name) => own_size += file_size.parse::<u32>().unwrap(),
+        if let ["cd", dir_name] = input_and_output.split_whitespace().collect_vec().as_slice() {
+            match *dir_name {
+                ".." => current_path = current_path.parent().unwrap().to_path_buf(),
+                _ => current_path.push(dir_name),
+            }
+        } else {
+            let (_input, output) = input_and_output.split_once("\n").unwrap();
+            for line in output.lines() {
+                match line.split_once(" ").unwrap() {
+                    ("dir", _dir_name) => continue,
+                    (file_size, file_name) => {
+                        let file_size_parsed = file_size.parse::<u32>().unwrap();
+                        let file_path = current_path.join(file_name);
+                        files_with_sizes.insert(file_path, file_size_parsed);
                     }
                 }
-                let dir_name = dir_stack.last().unwrap();
-                let dir = Directory::build(subdirs, own_size);
-                filesystem.entry(dir_name).or_insert(dir);
             }
         }
     }
 
-    fn get_dir_size<'a>(dir_name: &str, filesystem: &'a HashMap<&str, Directory>) -> u32 {
-        let dir = &filesystem[dir_name];
-        let size = dir.own_size
-            + if let [] = dir.subdirs[..] {
-                0
-            } else {
-                dir.subdirs
-                    .iter()
-                    .map(|subdir_name| get_dir_size(subdir_name, filesystem))
-                    .sum()
-            };
-        size
+    let mut dirs_with_sizes: HashMap<&Path, u32> = HashMap::new();
+
+    for (file_path, file_size) in &files_with_sizes {
+        for ancestor in file_path.ancestors() {
+            dirs_with_sizes
+                .entry(ancestor)
+                .and_modify(|dir_size| *dir_size += file_size)
+                .or_insert(*file_size);
+        }
     }
 
-    filesystem
-        .keys()
-        .map(|dir_name| get_dir_size(dir_name, &filesystem))
-        .filter(|dir_size| *dir_size <= 100000)
+    dirs_with_sizes
+        .values()
+        .filter(|dir_size| **dir_size <= upper_bound)
         .sum()
 }
 
