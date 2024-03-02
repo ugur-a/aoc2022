ca90b63e05c72a0fbbeb5616d5b2ed5675687b5d
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-03-02 22:42:28 +0100

    (d.rs): add tests to the template file

--- src/d.rs
+++ src/d.rs
@@ -4,3 +4,30 @@ pub fn p1(file: &str) -> () {
 pub fn p2(file: &str) -> () {
     todo!()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::read_to_string;
+    #[test]
+    fn test_p1() {
+        let inp = read_to_string("inputs/d10/test.txt").unwrap();
+        assert_eq!(p1(&inp), 21);
+    }
+    #[test]
+    fn real_p1() {
+        let inp = read_to_string("inputs/d10/real.txt").unwrap();
+        assert_eq!(p1(&inp), 1708);
+    }
+    #[test]
+    fn test_p2() {
+        let inp = read_to_string("inputs/d10/test.txt").unwrap();
+        assert_eq!(p2(&inp), 8);
+    }
+    #[test]
+    #[ignore]
+    fn real_p2() {
+        let inp = read_to_string("inputs/d10/real.txt").unwrap();
+        assert_eq!(p2(&inp), 504_000);
+    }
+}
