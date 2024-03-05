c81f510c631608ce1cb6c33bdcde3c8234afc1fa
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-03-05 08:44:11 +0100

    feat(d10): p1 done!

--- src/d10.rs
+++ src/d10.rs
@@ -1,6 +1,35 @@
-pub fn p1(file: &str) -> u32 {
-    todo!()
+use std::collections::HashMap;
+
+pub fn p1(file: &str) -> i32 {
+    let mut storage = HashMap::new();
+    //storage.insert(0, 1);
+    let mut current_cycle = 0;
+    let mut x = 1;
+    let interesting_cycles = Vec::from_iter((20..=220).step_by(40));
+    for line in file.lines() {
+        if let Some(("addx", num)) = line.split_once(' ') {
+            x += num.parse::<i32>().unwrap();
+            current_cycle += 2;
+            storage.insert(current_cycle, x);
+        // noop
+        } else {
+            current_cycle += 1;
+        }
+    }
+    let mut res = 0;
+    for mut cycle in interesting_cycles {
+        let init = cycle;
+        loop {
+            if let Some(value_of_x) = storage.get(&(cycle - 1)) {
+                res += init * value_of_x;
+                break;
+            }
+            cycle -= 1;
+        }
+    }
+    res
 }
+
 pub fn p2(file: &str) -> u32 {
     todo!()
 }
@@ -17,7 +46,7 @@ mod tests {
     #[test]
     fn real_p1() {
         let inp = read_to_string("inputs/d10/real.txt").unwrap();
-        assert_eq!(p1(&inp), 0);
+        assert_eq!(p1(&inp), 15360);
     }
     #[test]
     #[ignore]
