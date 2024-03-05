9eed4f6a9ca18aaf8e78d78e341f762c85b25484
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-03-05 12:51:28 +0100

    feat(d10): p2 done!

--- src/d10.rs
+++ src/d10.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use nohash_hasher::IntMap;
 
 pub fn p1(file: &str) -> i32 {
@@ -30,8 +31,58 @@ pub fn p1(file: &str) -> i32 {
         .sum()
 }
 
-pub fn p2(file: &str) -> u32 {
-    todo!()
+pub fn p2(file: &str) -> String {
+    let mut storage = IntMap::default();
+
+    let mut current_cycle = 0;
+    let mut x = 1;
+    storage.insert(current_cycle, x);
+    for line in file.lines() {
+        if let Some(("addx", num)) = line.split_once(' ') {
+            x += num.parse::<i32>().unwrap();
+            current_cycle += 2;
+            storage.insert(current_cycle, x);
+        // noop
+        } else {
+            current_cycle += 1;
+        }
+    }
+
+    struct Crt {
+        width: u32,
+        height: u32,
+    }
+
+    let crt = Crt {
+        width: 40,
+        height: 6,
+    };
+
+    (0..crt.height)
+        .map(|row_num| {
+            (0..crt.width)
+                .map(|col_num| {
+                    let cycle = crt.width * row_num + col_num;
+                    // only check against the horizontal position of the sprite
+                    let crt_position = col_num;
+
+                    let mut searcher = cycle;
+                    let center_of_sprite = loop {
+                        match storage.get(&searcher) {
+                            Some(value) => break value,
+                            None => searcher -= 1,
+                        }
+                    };
+
+                    if center_of_sprite.abs_diff(crt_position as i32) <= 1 {
+                        '#'
+                    } else {
+                        '.'
+                    }
+                })
+                .collect::<String>()
+        })
+        .join("\n")
 }
 
 #[cfg(test)]
@@ -49,15 +100,25 @@ mod tests {
         assert_eq!(p1(&inp), 15360);
     }
     #[test]
-    #[ignore]
     fn test_p2() {
         let inp = read_to_string("inputs/d10/test.txt").unwrap();
-        assert_eq!(p2(&inp), 8);
+        let out = "##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....";
+        assert_eq!(p2(&inp), out);
     }
     #[test]
-    #[ignore]
     fn real_p2() {
         let inp = read_to_string("inputs/d10/real.txt").unwrap();
-        assert_eq!(p2(&inp), 0);
+        let out = "###..#..#.#....#..#...##..##..####..##..
+#..#.#..#.#....#..#....#.#..#....#.#..#.
+#..#.####.#....####....#.#......#..#..#.
+###..#..#.#....#..#....#.#.##..#...####.
+#....#..#.#....#..#.#..#.#..#.#....#..#.
+#....#..#.####.#..#..##...###.####.#..#.";
+        assert_eq!(p2(&inp), out);
     }
 }
