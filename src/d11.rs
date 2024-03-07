11a03c99a2f601c12ede36866d791dcf5862fe88
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-03-07 17:40:16 +0100

    feat(d11): add operation parser

--- src/d11.rs
+++ src/d11.rs
@@ -1,5 +1,41 @@
+use std::str::FromStr;
+
 use anyhow::{bail, Context, Error, Result};
 
+
+enum Operation {
+    Add(u32),
+    Mul(u32),
+    Square,
+    Double,
+}
+
+impl FromStr for Operation {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut operation = s.split_whitespace();
+        let operand = operation.next().context("no operator")?;
+        let value = operation.next().context("no operand")?;
+        use Operation as O;
+        if let Ok(num) = value.parse::<u32>() {
+            match operand {
+                "+" => Ok(O::Add(num)),
+                "*" => Ok(O::Mul(num)),
+                _ => bail!("invalid operator"),
+            }
+        } else {
+            match value {
+                "old" => match operand {
+                    "+" => Ok(O::Double),
+                    "*" => Ok(O::Square),
+                    _ => bail!("invalid operator"),
+                },
+                _ => bail!("invalid operand"),
+            }
+        }
+    }
+}
 
 pub fn p1(file: &str) -> Result<u32> {
     todo!()
