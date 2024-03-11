85fe339968427d5b5d0870ccf0beb76b6546bfde
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-03-11 11:08:39 +0100

    (d): update the template to include anyhow and FromStr
    it seems like I use those in every day anyway

---
+++ src/d.rs
@@ -0,0 +1,38 @@
+use std::str::FromStr;
+
+use anyhow::{Error, Result};
+
+pub fn p1(file: &str) -> Result<u32> {
+    todo!()
+}
+pub fn p2(file: &str) -> Result<u32> {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::read_to_string;
+    #[test]
+    fn test_p1() {
+        let inp = read_to_string("inputs/d14/test.txt").unwrap();
+        assert_eq!(p1(&inp).unwrap(), 0);
+    }
+    #[test]
+    fn real_p1() {
+        let inp = read_to_string("inputs/d14/real.txt").unwrap();
+        assert_eq!(p1(&inp).unwrap(), 0);
+    }
+    #[test]
+    #[ignore]
+    fn test_p2() {
+        let inp = read_to_string("inputs/d14/test.txt").unwrap();
+        assert_eq!(p2(&inp).unwrap(), 0);
+    }
+    #[test]
+    #[ignore]
+    fn real_p2() {
+        let inp = read_to_string("inputs/d14/real.txt").unwrap();
+        assert_eq!(p2(&inp).unwrap(), 0);
+    }
+}
