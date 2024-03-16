5f194c524cd924851c6b97ea8a8ddb7a7e2f4f28
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-03-17 00:58:58 +0100

    feat(d15): p1 done! but slow on real input

--- src/d15.rs
+++ src/d15.rs
@@ -3,16 +3,16 @@ use std::{
     str::FromStr,
 };
 
-use anyhow::{Error, Result};
+use anyhow::{Context, Error, Result};
 use derive_deref::Deref;
 use itertools::Itertools;
 use rayon::{
-    iter::{IntoParallelRefIterator, ParallelIterator},
+    iter::{IntoParallelRefIterator, ParallelBridge, ParallelIterator},
     str::ParallelString,
 };
 use regex::Regex;
 
-#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 struct Point2D(i32, i32);
 
 impl Point2D {
@@ -89,8 +89,54 @@ pub fn p1(file: &str, analyzed_row_num: i32) -> Result<usize> {
     Ok(impossible_locations_of_distress_beacon.len())
 }
 
-pub fn p2(_file: &str) -> Result<u32> {
-    todo!()
+#[derive(Deref)]
+struct SensorsWithDistances(HashMap<SensorPosition, u32>);
+
+impl FromStr for SensorsWithDistances {
+    type Err = Error;
+
+    fn from_str(s: &str) -> std::prelude::v1::Result<Self, Self::Err> {
+        let coords_regex = Regex::new(
+            r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)",
+        )?;
+        let sensors_with_beacons = s
+            .par_lines()
+            .map(|line| {
+                coords_regex
+                    .captures_iter(line)
+                    .map(|caps| caps.extract().1.map(|coord| i32::from_str(coord).unwrap()))
+                    .exactly_one()
+                    .unwrap()
+            })
+            .map(|[sensor_x, sensor_y, beacon_x, beacon_y]| {
+                let sensor_coords = Point2D(sensor_x, sensor_y);
+                let beacon_coords = Point2D(beacon_x, beacon_y);
+                let distance = sensor_coords.manhattan_distance(beacon_coords);
+                (sensor_coords, distance)
+            })
+            .collect::<HashMap<_, _>>();
+
+        Ok(Self(sensors_with_beacons))
+    }
+}
+
+#[allow(clippy::cast_possible_wrap)]
+pub fn p2(file: &str, search_space_side_size: i32) -> Result<u64> {
+    let sensors_with_distances = SensorsWithDistances::from_str(file)?;
+    let distress_beacon = (0..=search_space_side_size)
+        .cartesian_product(0..=search_space_side_size)
+        .par_bridge()
+        .map(|(x, y)| Point2D(x, y))
+        .find_any(|point| {
+            sensors_with_distances
+                .par_iter()
+                .all(|(sensor, distance_to_nearest_beacon)| {
+                    sensor.manhattan_distance(*point) > *distance_to_nearest_beacon
+                })
+        })
+        .context("No distress beacon found")?;
+    let tuning_frequency: u64 = 4_000_000u64 * distress_beacon.0 as u64 + distress_beacon.1 as u64;
+    Ok(tuning_frequency)
 }
 
 #[cfg(test)]
@@ -108,13 +154,11 @@ mod tests {
         assert_eq!(p1(&inp, 2_000_000).unwrap(), 4748135);
     }
     #[test]
-    #[ignore]
     fn test_p2() {
         let inp = read_to_string("inputs/d15/test.txt").unwrap();
-        assert_eq!(p2(&inp).unwrap(), 0);
+        assert_eq!(p2(&inp, 20).unwrap(), 56_000_011);
     }
     #[test]
-    #[ignore]
     fn real_p2() {
         let inp = read_to_string("inputs/d15/real.txt").unwrap();
         assert_eq!(p2(&inp).unwrap(), 0);
