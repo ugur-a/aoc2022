e075c8b3fa2fc53105b7802e1d28f18779e271bb
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-03-16 15:10:25 +0100

    feat(d15): impl FromStr

--- src/d15.rs
+++ src/d15.rs
@@ -1,6 +1,45 @@
 use std::str::FromStr;
 
 use anyhow::{Error, Result};
+use itertools::Itertools;
+use regex::Regex;
+
+struct Point2D(i32, i32);
+
+struct Sensor {
+    coords: Point2D,
+    nearest_beacon_coords: Point2D,
+}
+
+struct Map {
+    sensors: Vec<Sensor>,
+}
+
+impl FromStr for Map {
+    type Err = Error;
+
+    fn from_str(s: &str) -> std::prelude::v1::Result<Self, Self::Err> {
+        let coords_regex = Regex::new(
+            r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)",
+        )?;
+        let sensors = s
+            .lines()
+            .map(|line| {
+                coords_regex
+                    .captures_iter(line)
+                    .map(|caps| caps.extract().1.map(|coord| i32::from_str(coord).unwrap()))
+                    .exactly_one()
+                    .unwrap()
+            })
+            .map(|[sensor_x, sensor_y, beacon_x, beacon_y]| Sensor {
+                coords: Point2D(sensor_x, sensor_y),
+                nearest_beacon_coords: Point2D(beacon_x, beacon_y),
+            })
+            .collect::<Vec<_>>();
+
+        Ok(Self { sensors })
+    }
+}
 
 pub fn p1(file: &str, row_to_analyze: usize) -> Result<usize> {
     todo!()
