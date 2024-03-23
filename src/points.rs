a2b0851efe4faae53939e20261d54e0b1022e236
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-03-23 15:05:18 +0100

    (points): impl Add for Point2D

--- src/points.rs
+++ src/points.rs
@@ -1,3 +1,5 @@
+use std::ops::Add;
+
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Default, Debug)]
 pub struct Point2D<T>(pub T, pub T);
 
@@ -12,5 +14,16 @@ impl<T: Copy> Point2D<T> {
 
     pub fn y(&self) -> T {
         self.1
+    }
+}
+
+impl<T> Add for Point2D<T>
+where
+    T: Add<Output = T>,
+{
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self(self.0 + rhs.0, self.1 + rhs.1)
     }
 }
