fff59d74698dd43d270b00c240e11650acb85398
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-03-25 22:27:12 +0100

    (d18): init

--- src/d18.rs
+++ src/d18.rs
@@ -1,8 +1,6 @@
-use std::str::FromStr;
+use anyhow::Result;
 
-use anyhow::{Error, Result};
-
-pub fn p1(file: &str) -> Result<u32> {
+pub fn p1(file: &str) -> Result<usize> {
     todo!()
 }
 pub fn p2(_file: &str) -> Result<u32> {
@@ -15,25 +13,25 @@ mod tests {
     use std::fs::read_to_string;
     #[test]
     fn test_p1() {
-        let inp = read_to_string("inputs/d19/test.txt").unwrap();
-        assert_eq!(p1(&inp).unwrap(), 0);
+        let inp = read_to_string("inputs/d18/test.txt").unwrap();
+        assert_eq!(p1(&inp).unwrap(), 64);
     }
     #[test]
     #[ignore]
     fn real_p1() {
-        let inp = read_to_string("inputs/d19/real.txt").unwrap();
+        let inp = read_to_string("inputs/d18/real.txt").unwrap();
         assert_eq!(p1(&inp).unwrap(), 0);
     }
     #[test]
     #[ignore]
     fn test_p2() {
-        let inp = read_to_string("inputs/d19/test.txt").unwrap();
+        let inp = read_to_string("inputs/d18/test.txt").unwrap();
         assert_eq!(p2(&inp).unwrap(), 0);
     }
     #[test]
     #[ignore]
     fn real_p2() {
-        let inp = read_to_string("inputs/d19/real.txt").unwrap();
+        let inp = read_to_string("inputs/d18/real.txt").unwrap();
         assert_eq!(p2(&inp).unwrap(), 0);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ pub mod d14;
 pub mod d15;
 //pub mod d16;
 pub mod d17;
-//pub mod d18;
+pub mod d18;
 //pub mod d19;
 pub mod d2;
 //pub mod d20;
