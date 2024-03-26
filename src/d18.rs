866734bd859ae640490a62789c675af6bbbdc29d
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-03-26 20:18:30 +0100

    (d18): p1 done!

--- src/d18.rs
+++ src/d18.rs
@@ -16,9 +16,32 @@ fn parse_droplet(s: &str) -> Result<DropletCube> {
 }
 
 pub fn p1(file: &str) -> Result<usize> {
-    todo!()
+    let droplet_cubes: Vec<DropletCube> = file
+        .lines()
+        .map(|line| parse_droplet(line))
+        .collect::<Result<Vec<_>, _>>()?;
+
+    // multiple droplets can have the same point as a potential exposed side (PES),
+    // so there will be duplicate values here
+    let num_exposed_sides: usize = droplet_cubes
+        .iter()
+        .flat_map(|&Point3D(x, y, z)| {
+            [
+                Point3D(x + 1, y, z),
+                Point3D(x, y + 1, z),
+                Point3D(x, y, z + 1),
+                Point3D(x - 1, y, z),
+                Point3D(x, y - 1, z),
+                Point3D(x, y, z - 1),
+            ]
+        })
+        .filter(|&potentially_exposed_side| !(droplet_cubes.contains(&potentially_exposed_side)))
+        .count();
+
+    Ok(num_exposed_sides)
 }
-pub fn p2(_file: &str) -> Result<u32> {
+
+pub fn p2(_file: &str) -> Result<usize> {
     todo!()
 }
 
@@ -32,16 +55,14 @@ mod tests {
         assert_eq!(p1(&inp).unwrap(), 64);
     }
     #[test]
-    #[ignore]
     fn real_p1() {
         let inp = read_to_string("inputs/d18/real.txt").unwrap();
-        assert_eq!(p1(&inp).unwrap(), 0);
+        assert_eq!(p1(&inp).unwrap(), 3526);
     }
     #[test]
-    #[ignore]
     fn test_p2() {
         let inp = read_to_string("inputs/d18/test.txt").unwrap();
-        assert_eq!(p2(&inp).unwrap(), 0);
+        assert_eq!(p2(&inp).unwrap(), 58);
     }
     #[test]
     #[ignore]
