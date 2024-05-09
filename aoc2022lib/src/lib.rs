bc66a16b7131c3a5837a1de5366f9ec6c43aa5ca
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-05-09 10:23:57 +0200

    mv lint rules to the workspace

--- aoc2022lib/src/lib.rs
+++ aoc2022lib/src/lib.rs
@@ -1,9 +1,3 @@
-#![warn(clippy::pedantic)]
-#![allow(
-    clippy::must_use_candidate,
-    clippy::missing_panics_doc,
-    clippy::missing_errors_doc
-)]
 #[macro_use]
 pub mod parse;
 pub mod points;
