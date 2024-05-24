a3e882b0fbff778d6945ee3b6ac5aab91d50df8a
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-05-24 23:12:02 +0200

    fix(parse): move use of trait deeper inside so that the macro can be called twice
    previously, when the macro was called +1 times in the same file/module,
    the +1 use statements would conflict

--- aoc2022lib/src/parse.rs
+++ aoc2022lib/src/parse.rs
@@ -7,12 +7,11 @@ pub fn n<N: std::str::FromStr>(input: &str) -> nom::IResult<&str, N> {
 #[macro_export]
 macro_rules! impl_from_str_from_nom_parser {
     ($fn:ident, $obj:ident) => {
-        use nom::Finish;
-
         impl std::str::FromStr for $obj {
             type Err = nom::error::Error<String>;
 
             fn from_str(s: &str) -> Result<Self, Self::Err> {
+                use nom::Finish;
                 match $fn(s).finish() {
                     Ok((_remaining, object)) => Ok(object),
                     Err(nom::error::Error { input, code }) => Err(Self::Err {
