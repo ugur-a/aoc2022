63529c31ca1523f7de39e2d542b0e069e0bd8bf4
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-08-03 18:30:49 +0200

    chore(d24): init

---
+++ d24/src/lib.rs
@@ -0,0 +1,37 @@
+pub fn p1(_file: &str) -> anyhow::Result<usize> {
+    todo!()
+}
+
+pub fn p2(_file: &str) -> anyhow::Result<u32> {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::read_to_string;
+
+    #[test]
+    fn test_p1() {
+        let inp = read_to_string("inputs/test.txt").unwrap();
+        assert_eq!(p1(&inp).unwrap(), 18);
+    }
+    #[test]
+    #[ignore]
+    fn real_p1() {
+        let inp = read_to_string("inputs/real.txt").unwrap();
+        assert_eq!(p1(&inp).unwrap(), 0);
+    }
+    #[test]
+    #[ignore]
+    fn test_p2() {
+        let inp = read_to_string("inputs/test.txt").unwrap();
+        assert_eq!(p2(&inp).unwrap(), 0);
+    }
+    #[test]
+    #[ignore]
+    fn real_p2() {
+        let inp = read_to_string("inputs/real.txt").unwrap();
+        assert_eq!(p2(&inp).unwrap(), 0);
+    }
+}
